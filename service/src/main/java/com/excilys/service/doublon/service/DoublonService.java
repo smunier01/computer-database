02282f27eb89362f9a80e4a2a605b59babc2204b
fcastelain <fcastelain@excilys.com>
2016-06-09 17:34:39 +0200

    set the problem of service

--- service/src/main/java/com/excilys/service/doublon/service/DoublonService.java
+++ service/src/main/java/com/excilys/service/doublon/service/DoublonService.java
@@ -1,24 +1,30 @@
 package com.excilys.service.doublon.service;
 
+import com.excilys.binding.doublon.Hamming;
+import com.excilys.binding.doublon.Levenshtein;
+import com.excilys.binding.doublon.SimilarityCalculator;
+import com.excilys.binding.mapper.DTOMapper;
 import com.excilys.core.doublon.model.Rapport;
 import com.excilys.core.dto.ComputerDTO;
 import com.excilys.service.service.impl.ComputerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public class DoublonService {
 
-    @Autowired
     private ComputerService computerService;
+    private DTOMapper mapper;
+
+
+    private SimilarityCalculator hamming = new Hamming();
+    private SimilarityCalculator levenshtein = new Levenshtein();
 
     public Rapport getRapport(List<ComputerDTO> computers) {
+        List<ComputerDTO> computersInBase = mapper.toDTO(this.computerService.getAll());
         Rapport retVal = new Rapport();
-
-
-
+        for (ComputerDTO cTemp : computers) {
+            // TODO: to implements
+        }
         return retVal;
     }
 
